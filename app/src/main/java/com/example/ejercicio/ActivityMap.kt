5f08df0530200bb3a5d95292821dc52daed354f4
MiguelAndresPalaciosCardenas <a.mapc49@gmail.com>
2022-03-02 10:09:09 -0500

    Revert "Version 0.3"
    This reverts commit f0c733034ba37d9e1b547e3c234705149047b516.

--- app/src/main/java/com/example/ejercicio/ActivityMap.kt
+++ app/src/main/java/com/example/ejercicio/ActivityMap.kt
@@ -1,42 +1,11 @@
 package com.example.ejercicio
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_map.*
 
-private const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
-
-class ActivityMap : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var map: GoogleMap
-
+class ActivityMap : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        fab2.setOnClickListener {
-            Toast.makeText(this, "Volviendo al inicio", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, MainActivity::class.java).apply {
-                putExtra(EXTRA_MESSAGE, "Error")
-            }
-            startActivity(intent)
-        }
-        createFragment()
-    }
-
-    private fun createFragment() {
-        val mapFragment: SupportMapFragment =
-            supportFragmentManager.findFragmentById(R.id.fMap) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
     }
 }
--- app/src/main/java/com/example/ejercicio/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.ejercicio
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,8 +11,6 @@ import com.example.ejercicio.adapter.PeopleAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 
-private const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,18 +18,14 @@ class MainActivity : AppCompatActivity() {
         //PeopleProvider.peopleList
         setContentView(R.layout.activity_main)
         initRecyclerView()
-        fab.setOnClickListener {
-            Toast.makeText(this, "Accediendo al mapa", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ActivityMap::class.java).apply {
-                putExtra(EXTRA_MESSAGE, "Error")
-            }
-            startActivity(intent)
+        fab.setOnClickListener{
+            Toast.makeText(this,"Holalaaaaaa",Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun initRecyclerView() {
-        val manager = LinearLayoutManager(this)
-        val decoration = DividerItemDecoration(this, manager.orientation)
+        val manager=LinearLayoutManager(this)
+        val decoration=DividerItemDecoration(this,manager.orientation)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerTest)
         recyclerView.layoutManager = manager
         recyclerView.adapter = PeopleAdapter(PeopleProvider.peopleList)
