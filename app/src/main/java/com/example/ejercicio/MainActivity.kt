eea1b5c451e52793554d03013637c955a27edc6a
MiguelAndresPalaciosCardenas <a.mapc49@gmail.com>
2022-03-03 12:17:09 -0500

    Version 0.5
    Conect firebase

--- app/src/main/java/com/example/ejercicio/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio/MainActivity.kt
@@ -1,26 +1,62 @@
 package com.example.ejercicio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ejercicio.adapter.PeopleAdapter
+import com.example.ejercicio.databinding.ActivityMainBinding
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    //lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //PeopleProvider.peopleList
         setContentView(R.layout.activity_main)
         initRecyclerView()
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+        setUpFab()
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
+    }
+
+    private fun setUpFab() {
+        //val fab = binding.fab
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerTest)
+        fab.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, "Error")
+            }
+            startActivity(intent)
+        }
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0) {
+                    fab.collapse()
+                } else {
+                    fab.expand()
+                }
+            }
+        })
     }
 
     private fun initRecyclerView() {
-        val manager=LinearLayoutManager(this)
-        val decoration=DividerItemDecoration(this,manager.orientation)
+        val manager = LinearLayoutManager(this)
+        val decoration = DividerItemDecoration(this, manager.orientation)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerTest)
         recyclerView.layoutManager = manager
         recyclerView.adapter = PeopleAdapter(PeopleProvider.peopleList)
--- app/src/main/java/com/example/ejercicio/PeopleProvider.kt
+++ app/src/main/java/com/example/ejercicio/PeopleProvider.kt
@@ -51,6 +51,27 @@ class PeopleProvider {
                 80,
                 "+57 3206211234",
                 false
+            ),
+            People(
+                "",
+                "Julio Palacios",
+                52,
+                "+57 3212345672",
+                false
+            ),
+            People(
+                "https://d2yoo3qu6vrk5d.cloudfront.net/images/20220217155204/cropped-amparo-grisales-joven-la-transformacion-fisica-de-la-actriz-con-los-anos-11.webp",
+                "Amparo Grisales",
+                200,
+                "",
+                true
+            ),
+            People(
+                "https://img.lalr.co/cms/2021/12/12085250/Vicente-Fern%C3%A1ndez.jpg?size=xl",
+                "Vicente Fernandez",
+                80,
+                "+57 3206211234",
+                false
             )
         )
     }
